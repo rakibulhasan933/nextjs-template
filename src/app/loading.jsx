6c9030866c5392c56e08e8ffe44ed496fa79836e
rakibulhasan933 <rakibulmd933@gmail.com>
2025-03-05 09:53:17 +0600

    final setup

---
+++ src/app/loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function loading() {
+    return (
+        <div className=' flex justify-center items-center'>
+            <span className="loading loading-ring loading-xs"></span>
+            <span className="loading loading-ring loading-sm"></span>
+            <span className="loading loading-ring loading-md"></span>
+            <span className="loading loading-ring loading-lg"></span>
+            <span className="loading loading-ring loading-xl"></span>
+        </div>
+    )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,7 @@
-import Count from "@/components/Count";
-import Hero from "@/components/Hero";
-import Navbar from "@/components/Navbar";
-import Services from "@/components/Services";
-import Works from "@/components/Works";
+
 
 export default function Home() {
   return (
-    <section className=" mx-20 my-5">
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <Count></Count>
-      <Services />
-      <Works></Works>
-    </section>
+    <div>Hello World</div>
   );
 }
